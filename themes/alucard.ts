fa470226776eb64140c5f38df384255ba2eef9f7
MidnightChems <wesorazem@gmail.com>
2026-06-26 15:44:07 -0600

    Updated alucard activeTabText to use the old background color, not foreground.

--- themes/alucard.ts
+++ themes/alucard.ts
@@ -41,7 +41,7 @@ export const alucardTheme: ThemeDefinition = {
     markupDelimiter: "2A6B45",
     markupAttribute: "036A96",
     tagAttributeChrome: "C5CDE0",
-    activeTabText: "1F1F1F",
+    activeTabText: "FFFBEB",
   },
   fontName: "Consolas",
   fontSize: "10",
